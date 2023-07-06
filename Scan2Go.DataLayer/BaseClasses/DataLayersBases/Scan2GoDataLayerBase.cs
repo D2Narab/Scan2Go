4902e5a9d5cb36e4764a5cefe9c65a11789155f8
Naser ARAB <narab@d2ecosystems.com>
2023-07-06 11:26:05 +0300

    Added base classes for datalayer base.

---
+++ Scan2Go.DataLayer/BaseClasses/DataLayersBases/Scan2GoDataLayerBase.cs
@@ -0,0 +1,18 @@
+﻿using Scan2Go.Enums;
+using DataLayer.Base.DatabaseFactory;
+
+namespace Scan2Go.DataLayer.BaseClasses.DataLayersBases;
+
+public class Scan2GoDataLayerBase : DataLayerBase
+{
+    public Scan2GoDataLayerBase() : base(DbNames.Default)
+    {
+
+    }
+
+
+
+    public Scan2GoDataLayerBase(DataLayerBase dataLayerBase) : base(dataLayerBase)
+    {
+    }
+}
---
+++ Scan2Go.DataLayer/BaseClasses/SelectOperationBases/Scan2GoSelectOperations.cs
@@ -0,0 +1,17 @@
+﻿using Scan2Go.Enums;
+using DataLayer.Base.DatabaseFactory;
+using DataLayer.Base.GeneralDataLayer;
+
+namespace Scan2Go.DataLayer.BaseClasses.SelectOperationBases;
+
+public class Scan2GoSelectOperations : GeneralSelectOperations
+{
+    public Scan2GoSelectOperations(DataLayerBase dataLayerBase) : base(dataLayerBase)
+    {
+    }
+
+    public Scan2GoSelectOperations() : base(DbNames.Default)
+    {
+
+    }
+}
