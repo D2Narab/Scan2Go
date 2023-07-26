ce24ce348effe175d057c51810e8c20b0973822f
Naser ARAB <narab@d2ecosystems.com>
2023-07-26 15:31:23 +0300

    Started to do global endpoint infrastructure.

---
+++ Scan2Go.Enums/Endpoints.cs
@@ -0,0 +1,23 @@
+﻿using Utility.Core.DataLayer;
+
+namespace Scan2Go.Enums;
+
+public class Endpoints : DataLayerEnumBase
+{
+    public const string endpointAddress = "https://localhost:44387/";
+    #region Users
+
+    public static readonly DataLayerEnumBase GetUsersList = new Endpoints("Users/GetUsersList");
+
+    #endregion Users
+
+    public Endpoints(string internalValue) : base(internalValue)
+    {
+        internalValue = endpointAddress + internalValue;
+    }
+
+    public static implicit operator string(Endpoints dataLayerEnumBase)
+    {
+        return dataLayerEnumBase.InternalValue;
+    }
+}
