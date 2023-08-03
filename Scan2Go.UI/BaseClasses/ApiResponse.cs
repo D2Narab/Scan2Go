9051213c41b2f467d5c9d508ff38261a4660e4c9
Naser ARAB <narab@d2ecosystems.com>
2023-08-03 15:53:02 +0300

    Made extentions for the Json Element and the list is completly populated dynamically.

---
+++ Scan2Go.UI/BaseClasses/ApiResponse.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+using Utility.Bases;
+
+namespace Scan2Go.UI.BaseClasses
+{
+    public class ApiResponse
+    {
+        [JsonPropertyName("resultObject")]
+        public ResultObject ResultObject { get; set; } = new();
+    }
+
+    public class ResultObject
+    {
+        [JsonPropertyName("listItemBases")]
+        public List<dynamic> ListItemBases { get; set; } = new();
+
+        [JsonPropertyName("listCaptionBases")]
+        public List<ListCaptionBase> ListCaptionBases { get; set; } = new();
+
+        [JsonPropertyName("recordInfo")]
+        public string? RecordInfo { get; set; }
+
+        [JsonPropertyName("totalRecordCount")]
+        public int TotalRecordCount { get; set; }
+    }
+}
