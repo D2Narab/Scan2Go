33c3a8d4fae166bb3c9f2532ed5cbcfe6185f69d
Naser ARAB <naserarab007@gmail.com>
2023-09-05 19:08:49 +0300

    Sent the Base64 to an API and got the response as dynamic and also extracted data from that response.

--- Scan2Go.MailReader/Program.cs
+++ Scan2Go.MailReader/Program.cs
@@ -5,10 +5,14 @@ using MimeKit;
 using System;
 using System.Net;
 using System.Net.Mail;
+using System.Net.Http;
+using System.Text;
+using Org.BouncyCastle.Asn1.Ocsp;
+using Newtonsoft.Json.Linq;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         string email = "d2demo@outlook.com";
         string password = "d2eco!!!";
@@ -64,6 +68,121 @@ class Program
                                 File.WriteAllText(filePath, base64Pdf);
 
                                 Console.WriteLine("Text written to file: " + filePath);
+
+                                /******************************************* API Calling ***************************************/
+                                // Create the JSON request object
+                                var request = new
+                                {
+                                    processParam = new
+                                    {
+                                        scenario = "FullProcess"
+                                    },
+                                    List = new[]
+                                    {
+                new
+                {
+                    format = ".jpg",
+                    light = 6,
+                    page_idx = 0,
+                    ImageData = new
+                    {
+                        image = base64Pdf
+                    }
+                }
+            }
+                                };
+
+                                // Serialize the request object to JSON
+                                string jsonRequest = Newtonsoft.Json.JsonConvert.SerializeObject(request);
+
+                                // Define the API endpoint URL
+                                string apiUrl = "http://localhost:8080/api/process";
+
+                                dynamic responseData = null;
+                                using (var httpClient = new HttpClient())
+                                {
+                                    // Create a StringContent with the JSON data
+                                    var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+                                    // Send the POST request
+                                    HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
+
+                                    // Check if the request was successful
+                                    if (response.IsSuccessStatusCode)
+                                    {
+                                        // Read the response content as a dynamic object
+                                        responseData = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(await response.Content.ReadAsStringAsync());
+
+                                        // You can now work with the responseData as needed
+                                        Console.WriteLine("API Response:");
+                                        Console.WriteLine(responseData);
+
+                                        // Example: Access a specific property in the response
+                                        var someProperty = responseData.List;
+                                        Console.WriteLine($"Some Property: {someProperty}");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine($"API Request failed with status code: {response.StatusCode}");
+                                    }
+                                }
+
+                                /******************************************* API Calling ***************************************/
+
+                                /***************************************** Finding values **************************************/
+                                string fieldNameToFind = "Document Number";
+                                string bufTextValue = FindBufTextValue(responseData, fieldNameToFind);
+
+                                if (bufTextValue != null)
+                                {
+                                    Console.WriteLine($"Found FieldName: '{fieldNameToFind}', Buf_Text Value: '{bufTextValue}'");
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"Field with FieldName '{fieldNameToFind}' not found.");
+                                }
+
+                                // Recursively find the Buf_Text value associated with a specific FieldName
+                                static string FindBufTextValue(dynamic data, string fieldName)
+                                {
+                                    if (data is JObject jsonObject)
+                                    {
+                                        foreach (var property in jsonObject.Properties())
+                                        {
+                                            if (property.Name == "FieldName" && property.Value.ToString() == fieldName)
+                                            {
+                                                // Search for Buf_Text in the same level
+                                                var bufTextProperty = jsonObject.Property("Buf_Text");
+                                                if (bufTextProperty != null)
+                                                {
+                                                    return bufTextProperty.Value.ToString();
+                                                }
+                                            }
+                                            else
+                                            {
+                                                var result = FindBufTextValue(property.Value, fieldName);
+                                                if (result != null)
+                                                {
+                                                    return result;
+                                                }
+                                            }
+                                        }
+                                    }
+                                    else if (data is JArray jsonArray)
+                                    {
+                                        foreach (var item in jsonArray)
+                                        {
+                                            var result = FindBufTextValue(item, fieldName);
+                                            if (result != null)
+                                            {
+                                                return result;
+                                            }
+                                        }
+                                    }
+
+                                    return null;
+                                }
+                                /***************************************** Finding values **************************************/
                             }
                             catch (Exception ex)
                             {
