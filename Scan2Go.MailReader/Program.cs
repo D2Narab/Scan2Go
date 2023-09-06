4c5a8338419b48efc39e4c76d5e5bf5fada5d310
Naser ARAB <narab@d2ecosystems.com>
2023-09-06 10:41:24 +0300

    Refactoring mail reading operations and added new methods to the framework.

--- Scan2Go.MailReader/Program.cs
+++ Scan2Go.MailReader/Program.cs
@@ -3,201 +3,182 @@ using MailKit.Net.Imap;
 using MailKit.Search;
 using MimeKit;
 using System;
+using System.Collections;
 using System.Net;
 using System.Net.Mail;
 using System.Net.Http;
 using System.Text;
+using MailReader;
 using Org.BouncyCastle.Asn1.Ocsp;
 using Newtonsoft.Json.Linq;
+using Utility;
 
 class Program
 {
     static async Task Main(string[] args)
     {
-        string email = "d2demo@outlook.com";
-        string password = "d2eco!!!";
-        string imapServer = "outlook.office365.com"; // IMAP server address
-        int port = 993; // IMAP port
-        bool useSsl = true;
+        //string email = "d2demo@outlook.com";
+        //string password = "d2eco!!!";
+        //string imapServer = "outlook.office365.com"; // IMAP server address
+        //int port = 993; // IMAP port
+        //bool useSsl = true;
 
-        using (var client = new ImapClient())
+        //IList<MailKit.UniqueId> mails;
+        //IMailFolder inbox;
+
+        //using (var client = new ImapClient())
+        //{
+        //    await client.ConnectAsync(imapServer, port, useSsl);
+        //    await client.AuthenticateAsync(email, password);
+
+        //    inbox = client.Inbox;
+        //    await inbox.OpenAsync(FolderAccess.ReadOnly);
+
+        //    // Retrieve unread messages from the inbox
+        //    mails = await inbox.SearchAsync(SearchQuery.NotSeen);
+        //    await client.DisconnectAsync(true);
+        //}
+
+        MailReadingResults mailReadingResults = await MailReader.MailReader.ReadMail(new MailReadingProperties
         {
-            client.Connect(imapServer, port, useSsl);
-            client.Authenticate(email, password);
+            EmailAddress = "d2demo@outlook.com",
+            Password = "d2eco!!!",
+            ImapServer = "outlook.office365.com",
+            Port = 993,
+            UseSsl = true,
+            SearchQuery = SearchQuery.NotSeen,
+            DisconnectClient = false
+        });
 
-            var inbox = client.Inbox;
-            inbox.Open(FolderAccess.ReadOnly);
+        foreach (var mailId in mailReadingResults.Mails)
+        {
+            var message = await mailReadingResults.Inbox.GetMessageAsync(mailId);
 
-            // Retrieve unread messages from the inbox
-            var messages = inbox.Search(SearchQuery.NotSeen);
+            Console.WriteLine($"Subject: {message.Subject}");
+            Console.WriteLine($"From: {message.From}");
+            Console.WriteLine($"Date: {message.Date}");
 
-            foreach (var uid in messages)
+            foreach (var attachment in message.Attachments)
             {
-                var message = inbox.GetMessage(uid);
-
-                Console.WriteLine($"Subject: {message.Subject}");
-                Console.WriteLine($"From: {message.From}");
-                Console.WriteLine($"Date: {message.Date}");
-
-                foreach (var attachment in message.Attachments)
+                if (attachment is not MimePart mimePart ||
+                    mimePart.FileName.EndsWith(".pdf", StringComparison.OrdinalIgnoreCase) == false)
                 {
-                    if (attachment is MimePart mimePart && mimePart.FileName.EndsWith(".pdf", StringComparison.OrdinalIgnoreCase))
-                    {
-                        using (MemoryStream memoryStream = new MemoryStream())
-                        {
-                            mimePart.Content.DecodeTo(memoryStream);
+                    Console.WriteLine("Error: Attachment is not a PDF.");
 
-                            byte[] pdfBytes = memoryStream.ToArray();
-                            string base64Pdf = Convert.ToBase64String(pdfBytes);
-
-                            // Do something with the base64 PDF data
-                            Console.WriteLine("Attachment is a PDF:");
-                            Console.WriteLine(base64Pdf);
-
-                            // Specify the folder path and file name
-                            string folderPath = @"D:\D2\Scan2Go\MailImports"; // Change this to your desired folder path
-                            //string fileName = $"{message.Date.ToString().Replace('/','_')} {mimePart.FileName}.txt";
-                            string fileName = "output.txt";
-
-                            // Combine the folder path and file name to create the full file path
-                            string filePath = Path.Combine(folderPath, fileName);
-
-                            try
-                            {
-                                // Write the text to the file
-                                File.WriteAllText(filePath, base64Pdf);
-
-                                Console.WriteLine("Text written to file: " + filePath);
-
-                                /******************************************* API Calling ***************************************/
-                                // Create the JSON request object
-                                var request = new
-                                {
-                                    processParam = new
-                                    {
-                                        scenario = "FullProcess"
-                                    },
-                                    List = new[]
-                                    {
-                new
-                {
-                    format = ".jpg",
-                    light = 6,
-                    page_idx = 0,
-                    ImageData = new
-                    {
-                        image = base64Pdf
-                    }
+                    continue;
                 }
-            }
-                                };
 
-                                // Serialize the request object to JSON
-                                string jsonRequest = Newtonsoft.Json.JsonConvert.SerializeObject(request);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    await mimePart.Content.DecodeToAsync(memoryStream);
 
-                                // Define the API endpoint URL
-                                string apiUrl = "http://localhost:8080/api/process";
+                    byte[] pdfBytes = memoryStream.ToArray();
+                    string base64Pdf = Convert.ToBase64String(pdfBytes);
 
-                                dynamic responseData = null;
-                                using (var httpClient = new HttpClient())
-                                {
-                                    // Create a StringContent with the JSON data
-                                    var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+                    // Do something with the base64 PDF data
+                    //Console.WriteLine("Attachment is a PDF:");
+                    //Console.WriteLine(base64Pdf);
 
-                                    // Send the POST request
-                                    HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
+                    // Specify the folder path and file name
+                    //string folderPath = @"D:\D2\Scan2Go\MailImports"; // Change this to your desired folder path
+                    //string fileName = $"{message.Date.ToString().Replace('/','_')} {mimePart.FileName}.txt";
+                    //string fileName = "output.txt";
 
-                                    // Check if the request was successful
-                                    if (response.IsSuccessStatusCode)
-                                    {
-                                        // Read the response content as a dynamic object
-                                        responseData = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(await response.Content.ReadAsStringAsync());
+                    // Combine the folder path and file name to create the full file path
+                    //string filePath = Path.Combine(folderPath, fileName);
 
-                                        // You can now work with the responseData as needed
-                                        Console.WriteLine("API Response:");
-                                        Console.WriteLine(responseData);
+                    try
+                    {
+                        // Write the text to the file
+                        //File.WriteAllText(filePath, base64Pdf);
 
-                                        // Example: Access a specific property in the response
-                                        var someProperty = responseData.List;
-                                        Console.WriteLine($"Some Property: {someProperty}");
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine($"API Request failed with status code: {response.StatusCode}");
-                                    }
-                                }
+                        //Console.WriteLine("Text written to file: " + filePath);
 
-                                /******************************************* API Calling ***************************************/
-
-                                /***************************************** Finding values **************************************/
-                                string fieldNameToFind = "Document Number";
-                                string bufTextValue = FindBufTextValue(responseData, fieldNameToFind);
-
-                                if (bufTextValue != null)
-                                {
-                                    Console.WriteLine($"Found FieldName: '{fieldNameToFind}', Buf_Text Value: '{bufTextValue}'");
-                                }
-                                else
-                                {
-                                    Console.WriteLine($"Field with FieldName '{fieldNameToFind}' not found.");
-                                }
-
-                                // Recursively find the Buf_Text value associated with a specific FieldName
-                                static string FindBufTextValue(dynamic data, string fieldName)
-                                {
-                                    if (data is JObject jsonObject)
-                                    {
-                                        foreach (var property in jsonObject.Properties())
-                                        {
-                                            if (property.Name == "FieldName" && property.Value.ToString() == fieldName)
-                                            {
-                                                // Search for Buf_Text in the same level
-                                                var bufTextProperty = jsonObject.Property("Buf_Text");
-                                                if (bufTextProperty != null)
-                                                {
-                                                    return bufTextProperty.Value.ToString();
-                                                }
-                                            }
-                                            else
-                                            {
-                                                var result = FindBufTextValue(property.Value, fieldName);
-                                                if (result != null)
-                                                {
-                                                    return result;
-                                                }
-                                            }
-                                        }
-                                    }
-                                    else if (data is JArray jsonArray)
-                                    {
-                                        foreach (var item in jsonArray)
-                                        {
-                                            var result = FindBufTextValue(item, fieldName);
-                                            if (result != null)
-                                            {
-                                                return result;
-                                            }
-                                        }
-                                    }
-
-                                    return null;
-                                }
-                                /***************************************** Finding values **************************************/
-                            }
-                            catch (Exception ex)
+                        /******************************************* API Calling ***************************************/
+                        // Create the JSON request object
+                        var request = new
+                        {
+                            processParam = new
                             {
-                                Console.WriteLine("An error occurred: " + ex.Message);
+                                scenario = "FullProcess"
+                            },
+                            List = new[]
+                            {
+                                new
+                                {
+                                    format = ".jpg",
+                                    light = 6,
+                                    page_idx = 0,
+                                    ImageData = new
+                                    {
+                                        image = base64Pdf
+                                    }
+                                }
+                            }
+                        };
+
+                        // Serialize the request object to JSON
+                        string jsonRequest = Newtonsoft.Json.JsonConvert.SerializeObject(request);
+
+                        // Define the API endpoint URL
+                        string apiUrl = "http://NASER-LNV:8080/api/process";
+
+                        dynamic responseData = null;
+                        using (var httpClient = new HttpClient())
+                        {
+                            // Create a StringContent with the JSON data
+                            var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+                            // Send the POST request
+                            HttpResponseMessage response = await httpClient.PostAsync(apiUrl, content);
+
+                            // Check if the request was successful
+                            if (response.IsSuccessStatusCode)
+                            {
+                                // Read the response content as a dynamic object
+                                responseData = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(await response.Content.ReadAsStringAsync());
+
+                                // You can now work with the responseData as needed
+                                //Console.WriteLine("API Response:");
+                                //Console.WriteLine(responseData);
+
+                                // Example: Access a specific property in the response
+                                //var someProperty = responseData.List;
+                                //Console.WriteLine($"Some Property: {someProperty}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"API Request failed with status code: {response.StatusCode}");
                             }
                         }
+
+                        /******************************************* API Calling ***************************************/
+
+                        /***************************************** Finding values **************************************/
+                        string fieldNameToFind = "Personal Number";
+                        //string bufTextValue = FindBufTextValue(responseData, fieldNameToFind);
+                        string bufTextValue = Utility.Extensions.PrimitiveExtensions
+                            .GetFieldValueInTheSameLevelOfAnotherField(responseData, "FieldName", fieldNameToFind, "Buf_Text");
+
+                        if (bufTextValue != null)
+                        {
+                            Console.WriteLine($"Found FieldName: '{fieldNameToFind}', Buf_Text Value: '{bufTextValue}'");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Field with FieldName '{fieldNameToFind}' not found.");
+                        }
+
+                        /***************************************** Finding values **************************************/
                     }
-                    else
+                    catch (Exception ex)
                     {
-                        Console.WriteLine("Error: Attachment is not a PDF.");
+                        Console.WriteLine("An error occurred: " + ex.Message);
                     }
                 }
             }
-
-            client.Disconnect(true);
         }
+
+        await mailReadingResults.ImapClient.DisconnectAsync(true);
     }
 }
