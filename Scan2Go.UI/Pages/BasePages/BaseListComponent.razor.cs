4f92376ee656fe752553b598b686081a949b6a21
Naser ARAB <narab@d2ecosystems.com>
2023-10-20 08:59:37 +0300

    We now have BaseListComponent to inherit from.

---
+++ Scan2Go.UI/Pages/BasePages/BaseListComponent.razor.cs
@@ -0,0 +1,13 @@
+﻿using Scan2Go.UI.BaseClasses;
+
+namespace Scan2Go.UI.Pages.BasePages
+{
+    public abstract partial class BaseListComponent
+    {
+        public PassingObject EntityListPassingObject { get; set; }
+        public abstract string PageTitle { get; set; }
+        public abstract Task HandleAddEntityClick();
+        public abstract Task OpenEntityEditPopup(int entityId);
+        public abstract void HandleEntitySaved();
+    }
+}
